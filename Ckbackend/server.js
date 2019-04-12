3f71bb819ebbee22b83df863f68d360201da658c
AbdullahShaaban <abdalahshaban@yahoo.com>
2019-04-12 18:50:45 +0200

    angular 2

--- Ckbackend/server.js
+++ Ckbackend/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
 const bodyparser = require('body-parser');
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 const port = 3000;
 
 const mongojs = require('mongojs');
-const db = mongojs('clientKeeper', ['clients']);
+const db = mongojs('clientKeeper', [ 'clients' ]);
 
 //set Statics folder
 
@@ -17,78 +17,79 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyparser.json());
 
 app.get('/', (req, res) => {
-    res.send('please use /api/clients')
-})
+	res.send('please use /api/clients');
+});
 
 //get Clents -Get
 app.get('/api/clients', (req, res) => {
-    db.clients.find().sort({
-        first_name: 1
-    }, (err, clients) => {
-        if (err) {
-            res.send(err)
-        } else {
-            res.json(clients)
-        }
-    })
+	db.clients.find().sort({
+		first_name: 1
+	}, (err, clients) => {
+		if (err) {
+			res.send(err);
+		} else {
+			res.json(clients);
+		}
+	});
 });
 
 //add Client
 app.post('/api/clients', (req, res) => {
-    console.log('first post');
+	console.log('first post');
 
-    db.clients.insert(req.body, (err, client) => {
-        if (err) {
-            res.send(err)
-        } else {
-            console.log('in insert');
+	db.clients.insert(req.body, (err, client) => {
+		if (err) {
+			res.send(err);
+		} else {
+			console.log('in insert');
 
-            res.json(client)
-        }
-    })
-})
+			res.json(client);
+		}
+	});
+});
 
 // update client
 app.put('/api/clients/:id', (req, res) => {
-    const id = req.params.id;
-    db.clients.findAndModify({
-        query: {
-            _id: mongojs.ObjectId(id)
-        },
-        update: {
-            $set: {
-                first_name: req.body.first_name,
-                last_name: req.body.last_name,
-                email: req.body.email,
-                phone: req.body.phone
-            }
-        },
-        new: true
-
-    }, (err, client) => {
-        res.json(client)
-    })
-})
+	const id = req.params.id;
+	db.clients.findAndModify(
+		{
+			query: {
+				_id: mongojs.ObjectId(id)
+			},
+			update: {
+				$set: {
+					first_name: req.body.first_name,
+					last_name: req.body.last_name,
+					email: req.body.email,
+					phone: req.body.phone
+				}
+			},
+			new: true
+		},
+		(err, client) => {
+			res.json(client);
+		}
+	);
+});
 
 //delete
 
 app.delete('/api/clients/:id', (req, res) => {
-    const id = req.params.id
-    db.clients.remove({
-        _id: mongojs.ObjectId(id)
-    }, (err, client) => {
-        if (err) {
-            res.send(err);
-        } else {
-            res.json(client)
-        }
-    })
-})
-
-
-
-
+	const id = req.params.id;
+	db.clients.remove(
+		{
+			_id: mongojs.ObjectId(id)
+		},
+		(err, client) => {
+			if (err) {
+				res.send(err);
+			} else {
+				res.json(client);
+			}
+		}
+	);
+});
 
 app.listen(port, () => {
-    console.log('server started on port' + port);
-})
+	console.log('server started on port' + port);
+});
